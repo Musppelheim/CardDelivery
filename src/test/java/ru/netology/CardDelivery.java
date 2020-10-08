900ab255f423d58b6979d6614fa9d06a885d9abc
Musppelheim <neloliver@hotmail.com>
2020-10-09 00:28:46 +0700

    meeting date added

--- src/test/java/ru/netology/CardDelivery.java
+++ src/test/java/ru/netology/CardDelivery.java
@@ -3,7 +3,10 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -15,7 +18,9 @@ public class CardDelivery {
         open("http://localhost:9999");
         SelenideElement form = $(".form");
         form.$("[data-test-id=city] input").setValue("Барнаул");
-        form.$("[data-test-id=date] input").setValue("05.10.2020");
+        String meetingDate = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        form.$("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        form.$("[placeholder=\"Дата встречи\"]").val(String.format(meetingDate, 8));
         form.$("[data-test-id=name] input").setValue("Нэя Сика");
         form.$("[data-test-id=phone] input").setValue("+79134568520");
         form.$("[data-test-id=agreement]").click();
